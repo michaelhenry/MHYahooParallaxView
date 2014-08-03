6719757401fa7bc4e3ee3bfd715da3d86a912e1e
Michael Henry Pantaleon <michaelhenry119@yahoo.com.ph>
2014-08-04 03:45:39 +0800

    update ViewControllers for the new changes

--- MHYahooParallaxViewExample/MHYahooParallaxViewExample/ViewController.m
+++ MHYahooParallaxViewExample/MHYahooParallaxViewExample/ViewController.m
@@ -1,84 +1,41 @@
 //
-// MHYahooParallaxView
-// iamkel.net
+//  ViewController.m
+//  CollectViewTest
 //
-//  Created by Michael Henry Pantaleon on 18/07/2014.
+//  Created by Michael Henry Pantaleon on 2/08/2014.
 //  Copyright (c) 2014 Michael Henry Pantaleon. All rights reserved.
 //
-// Version 1.0
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
-#import "ViewController.h"
-#import "MHYahooParallaxView.h"
 
 #define IMAGE_VIEW_TAG 100
 #define IMAGE_SCROLL_VIEW_TAG 101
-
 #define CONTENT_IMAGE_VIEW_TAG 102
 
-@interface ViewController ()<MHYahooParallaxViewDatasource,MHYahooParallaxViewDelegate,UITableViewDataSource,UITableViewDelegate>{
-    CGFloat _headerHeight;
-}
+#import "ViewController.h"
+#import "MHYahooParallaxView.h"
+#import "MHTsekotCell.h"
+//#import "MHYahooWeatherParallaxCell.h"
 
-
-@property (strong, nonatomic) MHYahooParallaxView *parallaxView;
-
-@end
-
-
-@interface TsekotCell : MHYahooParallaxViewCell<UITableViewDelegate> {
-    BOOL _mustDisableParent;
-}
-@property (nonatomic,strong) UITableView * tableView;
-
-- (id) initWithFrame:(CGRect)frame reuseIdentifier:(NSString*)reuseIdentifier withDataSource:(id<UITableViewDataSource>) datasource withDelegate:(id<UITableViewDelegate> ) delegate ;
-@end
-
-
-@implementation TsekotCell
-@synthesize tableView = _tableView;
-
-- (id) initWithFrame:(CGRect)frame reuseIdentifier:(NSString*)reuseIdentifier withDataSource:(id<UITableViewDataSource>) datasource withDelegate:(id<UITableViewDelegate> ) delegate {
-    self = [super initWithReuseIdentifier:reuseIdentifier withType:MHYahooParallaxViewCellTypeHorizontalCustom];
-    if(self) {
-        _tableView = [[UITableView alloc]initWithFrame:frame style:UITableViewStylePlain];
-        [self.contentView addSubview:_tableView];
-        _tableView.dataSource = datasource;
-        _tableView.delegate = delegate;
-
-    }
-    return self;
-}
-
-
-- (void) layoutSubviews {
-    [super layoutSubviews];
-}
-
-
+@interface ViewController ()<MHYahooParallaxViewDatasource,MHYahooParallaxViewDelegate,UITableViewDelegate,UITableViewDataSource>
 
 @end
-
 
 @implementation ViewController
-@synthesize parallaxView = _parallaxView;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    _imageHeaderHeight = self.view.frame.size.height * 0.70f;
+
+    MHYahooParallaxView * parallaxView = [[MHYahooParallaxView alloc]initWithFrame:CGRectMake(0.0f,0.0f, self.view.frame.size.width, self.view.frame.size.height)];
+    [parallaxView registerClass:[MHTsekotCell class] forCellWithReuseIdentifier:[MHTsekotCell reuseIdentifier]];
+    parallaxView.delegate = self;
+    parallaxView.datasource = self;
+    parallaxView.backgroundColor = [UIColor grayColor];
+    [self.view addSubview:parallaxView];
+    
+	// Do any additional setup after loading the view, typically from a nib.
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -87,46 +44,57 @@
 }
 
 
-- (void) loadView {
-    [super loadView];
-    _headerHeight = self.view.frame.size.height * 0.70f;
+- (UICollectionViewCell*) parallaxView:(MHYahooParallaxView *)parallaxView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    MHTsekotCell * tsekotCell = (MHTsekotCell*)[parallaxView dequeueReusableCellWithReuseIdentifier:[MHTsekotCell reuseIdentifier] forIndexPath:indexPath];
+    tsekotCell.delegate = self;
+    tsekotCell.datasource = self;
+    tsekotCell.tsekotTableView.tag = indexPath.row;
+    tsekotCell.tsekotTableView.contentOffset = CGPointMake(0.0f, 0.0f);
+    [tsekotCell.tsekotTableView reloadData];
 
-    _parallaxView = [[MHYahooParallaxView alloc]initWithFrame:CGRectMake(0.0f, 0.0f, self.view.frame.size.width, self.view.frame.size.height)];
-    _parallaxView.parallaxViewType = MHYahooParallaxViewTypeHorizontal;
-    _parallaxView.datasource = self;
-    _parallaxView.delegate = self;
-    _parallaxView.backgroundColor = [UIColor blackColor];
-
-    [self.view addSubview:_parallaxView];
-
+    return tsekotCell;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-}
 
-- (BOOL)shouldUseCustomCell {
-    return YES;
-}
-
-- (NSInteger) numberOfRows:(MHYahooParallaxView *)parallaxView {
+- (NSInteger) numberOfRowsInParallaxView:(MHYahooParallaxView *)parallaxView {
     return 10;
 }
 
-- (void)parallaxViewDidScrollHorizontally:(MHYahooParallaxView *)parallaxView leftIndex:(NSInteger)leftIndex leftImageLeftMargin:(CGFloat)leftImageLeftMargin leftImageWidth:(CGFloat)leftImageWidth rightIndex:(NSInteger)rightIndex rightImageLeftMargin:(CGFloat)rightImageLeftMargin rightImageWidth:(CGFloat)rightImageWidth  {
+- (void)parallaxViewDidScrollHorizontally:(MHYahooParallaxView *)parallaxView leftIndex:(NSInteger)leftIndex leftImageLeftMargin:(CGFloat)leftImageLeftMargin leftImageWidth:(CGFloat)leftImageWidth rightIndex:(NSInteger)rightIndex rightImageLeftMargin:(CGFloat)rightImageLeftMargin rightImageWidth:(CGFloat)rightImageWidth {
+
+    // leftIndex and Right Index should must be greater than or equal to zero
+
+    /*if(leftIndex >= 0){
+        MHYahooWeatherParallaxCell * leftCell = (MHYahooWeatherParallaxCell*)[parallaxView cellForItemAtIndexPath:[NSIndexPath indexPathForItem:leftIndex inSection:0]];
+
+        CGRect frame = leftCell.parallaxImageView.frame;
+        frame.origin.x = leftImageLeftMargin;
+        frame.size.width = leftImageWidth;
+        leftCell.parallaxImageView.frame = frame;
+    }
+    if(rightIndex >= 0){
+        MHYahooWeatherParallaxCell * rightCell = (MHYahooWeatherParallaxCell*)[parallaxView cellForItemAtIndexPath:[NSIndexPath indexPathForItem:rightIndex inSection:0]];
+        CGRect frame = rightCell.parallaxImageView.frame;
+        frame.origin.x = rightImageLeftMargin;
+        frame.size.width = rightImageWidth;
+        rightCell.parallaxImageView.frame = frame;
+    }*/
 
     if(leftIndex >= 0){
-        MHYahooParallaxViewCell * leftCell = [parallaxView cellForRowAtIndexPath:[NSIndexPath indexPathForItem:leftIndex inSection:0]];
-        UIImageView *iv = (UIImageView*)[leftCell viewWithTag:IMAGE_VIEW_TAG];
+        MHTsekotCell * leftCell = (MHTsekotCell*)[parallaxView cellForItemAtIndexPath:[NSIndexPath indexPathForItem:leftIndex inSection:0]];
+        UITableViewCell * tvCell = [leftCell.tsekotTableView cellForRowAtIndexPath:[NSIndexPath indexPathForItem:0 inSection:0]];
+
+        UIImageView *iv = (UIImageView*)[tvCell viewWithTag:100];
         CGRect frame = iv.frame;
         frame.origin.x = leftImageLeftMargin;
         frame.size.width = leftImageWidth;
         iv.frame = frame;
     }
     if(rightIndex >= 0){
-        MHYahooParallaxViewCell * rigthCell = [parallaxView cellForRowAtIndexPath:[NSIndexPath indexPathForItem:rightIndex inSection:0]];
-        UIImageView *iv = (UIImageView*)[rigthCell viewWithTag:IMAGE_VIEW_TAG];
+        MHTsekotCell * rigthCell = (MHTsekotCell*)[parallaxView cellForItemAtIndexPath:[NSIndexPath indexPathForItem:rightIndex inSection:0]];
+        UITableViewCell * tvCell = [rigthCell.tsekotTableView cellForRowAtIndexPath:[NSIndexPath indexPathForItem:0 inSection:0]];
+
+        UIImageView *iv = (UIImageView*)[tvCell viewWithTag:100];
         CGRect frame = iv.frame;
         frame.origin.x = rightImageLeftMargin;
         frame.size.width = rightImageWidth;
@@ -134,33 +102,22 @@
 
     }
 }
-- (MHYahooParallaxViewCell *)parallaxView:(MHYahooParallaxView *)parallaxView cellForRowAtIndexPath:(NSIndexPath *)indexPath  {
-    static NSString * cellID = @"custom";
-    TsekotCell * cell = (TsekotCell*)[parallaxView dequeueReusableCellWithIdentifier:cellID];
-    if(!cell)  {
-        cell = [[TsekotCell alloc]initWithFrame:CGRectMake(0.0f, 0.0f, self.view.frame.size.width, self.view.frame.size.height)  reuseIdentifier:cellID withDataSource:self withDelegate:self];
-    }
-
-    cell.tableView.tag = indexPath.row;
-    [cell.tableView reloadData];
-    [cell.tableView setContentOffset:CGPointMake(0.0f, 0.0f)];
-    return cell;
-}
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
 
--(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
     return 2;
 }
-
-- (CGFloat ) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath  {
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     if(indexPath.row == 0) {
-        return _headerHeight;
+        return _imageHeaderHeight;
     }
     return 568.0f;
 }
+
 
 - (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell * cell;
@@ -170,7 +127,7 @@
         if(!cell) {
             cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:headerId];
             cell.backgroundColor = [UIColor clearColor];
-            UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0.0f, 0.0f, cell.contentView.frame.size.width,_headerHeight)];
+            UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0.0f, 0.0f, cell.contentView.frame.size.width,_imageHeaderHeight)];
             imageView.contentMode = UIViewContentModeCenter;
             imageView.tag = IMAGE_VIEW_TAG;
             imageView.clipsToBounds = YES;
@@ -194,7 +151,7 @@
 
             CGRect headerFrame = headerInfo.frame;
             headerFrame.size.height = 149.0f;
-            headerFrame.origin.y = _headerHeight - 149.0f;
+            headerFrame.origin.y = _imageHeaderHeight - 149.0f;
             headerInfo.frame = headerFrame;
 
         }
@@ -223,7 +180,7 @@
     UITableViewCell * cell = [tv cellForRowAtIndexPath:[NSIndexPath indexPathForItem:0 inSection:0]];
     UIScrollView * svImage = (UIScrollView*)[cell viewWithTag:IMAGE_SCROLL_VIEW_TAG];
     CGRect frame = svImage.frame;
-    frame.size.height =  MAX((_headerHeight-tv.contentOffset.y),0);
+    frame.size.height =  MAX((_imageHeaderHeight-tv.contentOffset.y),0);
     frame.origin.y = tv.contentOffset.y;
     svImage.frame = frame;
     svImage.zoomScale = 1 + (abs(MIN(tv.contentOffset.y,0))/320.0f);
@@ -234,5 +191,6 @@
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
     return [scrollView viewWithTag:IMAGE_VIEW_TAG];
 }
+
 
 @end
